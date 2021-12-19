2ff4140724b99dcee5d882bad4fddf3337fffee3
mikeymizell <61428335+mikeymizell@users.noreply.github.com>
2021-12-19 15:15:14 -0800

    Add Heroku

---
+++ server.js
@@ -0,0 +1,54 @@
+const { animals } = require('./data/animals');
+const express = require('express');
+
+const app = express();
+
+app.listen(3001, () => {
+    console.log(`API server onw on port 3001!`);
+})
+
+app.get('/api/animals', (req, res) => {
+    let results = animals;
+
+    if (req.query) {
+        results = filterByQuery(req.query, results);
+    };
+
+    res.json(results);
+})
+
+function filterByQuery(query, animalsArray) {
+    let personalityTraitsArray = [];
+
+    //Note that we save the animalsArray as filteredResults here':
+    let filteredResults = animalsArray;
+
+    if (query.personalityTraits) {
+        if (typeof query.personalityTraits === 'string') {
+            personalityTraitsArray = [query.personalityTraits];
+        }
+        else {
+            personalityTraitsArray = query.personalityTraits;
+        }
+        
+        personalityTraitsArray.forEach(trait => {
+            filteredResults = filteredResults.filter(
+                animal => animal.personalityTraits.indexOf(trait) !== -1
+            );
+        });
+    }
+
+    if (query.diet) {
+        filteredResults = filteredResults.filter(animal => animal.diet === query.diet);
+    };
+    
+    if (query.species) {
+        filteredResults = filteredResults.filter(animal => animal.species === query.species);
+    };
+
+    if (query.name) {
+        filteredResults = filteredResults.filter(animal => animal.name === query.name);
+    };
+
+    return filteredResults;
+}
