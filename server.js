49c0fd8c14afaf786450c270b6087bc79478c345
mikeymizell <61428335+mikeymizell@users.noreply.github.com>
2021-12-19 15:39:22 -0800

    findById() function added using req.param

--- server.js
+++ server.js
@@ -8,16 +8,6 @@ app.listen(PORT, () => {
     console.log(`API server onw on port ${PORT}!`);
 })
 
-app.get('/api/animals', (req, res) => {
-    let results = animals;
-
-    if (req.query) {
-        results = filterByQuery(req.query, results);
-    };
-
-    res.json(results);
-})
-
 function filterByQuery(query, animalsArray) {
     let personalityTraitsArray = [];
 
@@ -53,3 +43,30 @@ function filterByQuery(query, animalsArray) {
 
     return filteredResults;
 }
+
+function findById(id, animalsArray) {
+    const result = animalsArray.filter(animal => animal.id === id)[0];
+    
+    return result;
+}
+
+app.get('/api/animals', (req, res) => {
+    let results = animals;
+
+    if (req.query) {
+        results = filterByQuery(req.query, results);
+    };
+
+    res.json(results);
+})
+
+app.get('/api/animals/:id', (req, res) => {
+    const result = findById(req.params.id, animals);
+    if (result) {
+        res.json(result);
+    }
+    else {
+        res.send(404);
+    };
+})
+
